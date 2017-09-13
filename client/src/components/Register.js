21312c24d55baa807618c634e1d54ed2ca6ae42c
Philip Zak <zakphi@gmail.com>
2017-09-13 17:22:31 -0400

    create register component

---
+++ client/src/components/Register.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Register = props => {
+  return(
+    <div className='registerForm'>
+      <form onSubmit={props.handleRegisterSubmit}>
+        <input
+          type='text'
+          name='registerFirstName'
+          value={props.registerFirstName}
+          onChange={props.handleInputChange}
+          placeholder='First Name'
+        />
+        <input
+          type='text'
+          name='registerLastName'
+          value={props.registerLastName}
+          onChange={props.handleInputChange}
+          placeholder='Last Name'
+        />
+        <input
+          type='text'
+          name='registerEmail'
+          value={props.registerEmail}
+          onChange={props.handleInputChange}
+          placeholder='Email'
+        />
+        <input
+          type='text'
+          name='registerUsername'
+          value={props.registerUsername}
+          onChange={props.handleInputChange}
+          placeholder='Username'
+        />
+        <input
+          type='password'
+          name='registerPassword'
+          value={props.registerPassword}
+          onChange={props.handleInputChange}
+          placeholder='Password'
+        />
+        <input type='submit' value='register' />
+      </form>
+    </div>
+  )
+}
+
+export default Register
