9ad9dddb0618756d53fea0eb9b7b0d472589e3ac
Philip Zak <zakphi@gmail.com>
2017-09-13 16:25:53 -0400

    import axios and rrd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import axios from 'axios'
+
 class App extends Component {
   render() {
     return (
