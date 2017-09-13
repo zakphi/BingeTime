f45255fc7ddaa154d7529fe7eb48586cbcd5430b
Philip Zak <zakphi@gmail.com>
2017-09-13 14:35:03 -0400

    create api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,26 @@
+class ApiController < ApplicationController
+  
+  def require_login
+    authenticate_token || render_unauthorized('access denied')
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  protected
+
+  def render_unauthorized
+    errors = { errors: [ detail: message ] }
+    render json: errors, status: :unauthorized
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token|
+      User.find_by(auth_token: token)
+    end
+  end
+
+end
