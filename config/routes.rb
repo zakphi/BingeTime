ab38c6199fe97fd698dcf8758306bcb69a8ac8ad
Philip Zak <zakphi@gmail.com>
2017-09-13 14:06:09 -0400

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/login' => 'sessions#create'
+  delete '/logoout' => 'sessions#destroy'
+  get '/profile' => 'users#profile'
+
+  resources :users
+  resources :tv_shows
 end
