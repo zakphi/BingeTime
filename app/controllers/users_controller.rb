21427b193f59bf872cd5255afde84ae54f0ac56c
Philip Zak <zakphi@gmail.com>
2017-09-13 14:55:32 -0400

    create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApiController
+  before_action :require_login, except: [:create]
+
+  def create
+    user = User.create!(user_params)
+    render json: { token: user.auth_token }
+  end
+
+  def profile
+    user = User.find_by_auth_token!(request.headers[:token])
+    render json: { user: { first_name: user.first_name, last_name: user.last_name, email: user.email, username: user.username } }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+  end
+end
