76ba74e2bdd5e9e8701839b40b8b3b0ac66cb5df
Philip Zak <zakphi@gmail.com>
2017-09-13 16:37:55 -0400

    wrote handleloginsubmit function

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     }
 
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
   }
 
   handleInputChange(e){
@@ -33,6 +34,28 @@ class App extends Component {
     this.setState({
       [name]: value
     })
+  }
+
+  handleLoginSubmit(e){
+    e.preventDefault()
+    axios.post('/login', {
+      username: this.state.loginUsername,
+      password: this.state.loginPassword
+    })
+      .then(res => {
+        console.log(res)
+        if(res.data.token){
+          Auth.authenticateToken(res.data.token)
+          this.setState({
+            auth: Auth.isUserAuthenticated(),
+            loginUsername: null,
+            loginPassword: null
+          })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
