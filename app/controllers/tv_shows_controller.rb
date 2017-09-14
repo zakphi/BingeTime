10df7b752f2e399a0057cfa724a5877d0be96481
Philip Zak <zakphi@gmail.com>
2017-09-14 14:54:17 -0400

    create tv shows controller

---
+++ app/controllers/tv_shows_controller.rb
@@ -0,0 +1,33 @@
+class TvShowsController < ApiController
+  before_action :require_login, except: [:index, :show]
+  
+  def index
+    tv_shows = TvShow.all
+    render json: { tv_shows: tv_shows }
+  end
+
+  def show
+    tv_show = TvShow.find(params[:id])
+    tv_show_user = tvshow.user
+    render json: { tv_show: tv_show, users: tvshow_user.username }
+  end
+
+  def create
+    tv_show = TvShow.new(tv_show_params)
+    # tv_show.user = current_user
+    if tv_show.save
+      render json: {
+        message: 'ok',
+        tv_show: tv_show
+      }
+    else
+      render json: { message: 'could not save show' }
+    end
+  end
+
+  private
+
+  def tv_show_params
+    params.require(:tv_show).permit(:external_id, :title, :summary, :poster_path, :run_time)
+  end
+end
