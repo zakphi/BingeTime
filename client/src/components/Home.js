a8972419329cc2913e7d0966aff07e2b02232bcc
Philip Zak <zakphi@gmail.com>
2017-09-14 18:15:13 -0400

    create home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Home = props => {
+  return(
+    <div>
+      <h1>home</h1>
+      <form>
+        <input type='text' name='showSearch' placeholder='Search for Show' />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
+
+export default Home
