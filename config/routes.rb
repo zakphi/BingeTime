99a57e1bec899c7ea58eb53dd7cf93ccd50c6c2c
Philip Zak <zakphi@gmail.com>
2017-09-15 14:38:16 -0400

    create route for search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get '/profile' => 'users#profile'
+  post '/search' => 'tv_shows#search'
 
   resources :users
   resources :tv_shows
