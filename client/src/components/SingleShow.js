be30b889807e6e13e4beff326a5cabf2dae3110d
Philip Zak <zakphi@gmail.com>
2017-09-16 17:05:04 -0400

    give single show div a background image from the show

--- client/src/components/SingleShow.js
+++ client/src/components/SingleShow.js
@@ -8,12 +8,26 @@ const SingleShow = props => {
   let minutes = Math.floor(totalMinutes - (days * 1440) - (hours * 60))
   let formattedBingeTime = `${days} days, ${hours} hours, and ${minutes} minutes`
 
+  let base_url = props.configResults.secure_base_url
+  let image_size = 'w1280'
+  let poster_path = props.singleShowData.backdrop_path
+  let poster_image = `${base_url}${image_size}${poster_path}`
+
+  const divStyle = {
+    backgroundImage: `url(${poster_image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  }
+
   return(
-    <div>
-      <h1>{props.singleShowData.original_name}</h1>
-      <p>{props.singleShowData.overview}</p>
-      <p>{props.singleShowData.original_name} has {props.singleShowData.number_of_seasons} seasons consisting of {props.singleShowData.number_of_episodes} episodes.</p>
-      <p>It will approximately take you {formattedBingeTime} to watch all of {props.singleShowData.original_name}</p>
+    <div style={divStyle}>
+      <div>
+        <h1>{props.singleShowData.original_name}</h1>
+        <p>{props.singleShowData.overview}</p>
+        <p>{props.singleShowData.original_name} has {props.singleShowData.number_of_seasons} seasons consisting of {props.singleShowData.number_of_episodes} episodes.</p>
+        <p>It will approximately take you {formattedBingeTime} to watch all of {props.singleShowData.original_name}</p>
+      </div>
     </div>
   )
 }
