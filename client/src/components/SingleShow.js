ca4a36e067d59f9f8e7cef0f65318328854e4079
Philip Zak <zakphi@gmail.com>
2017-09-16 13:22:01 -0400

    show more show data on single page

--- client/src/components/SingleShow.js
+++ client/src/components/SingleShow.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const SingleShow = props => {
+
+  let totalMinutes = props.singleShowData.number_of_episodes * props.singleShowData.episode_run_time[0]
+  let days = Math.floor(totalMinutes / 1440);
+  let hours = Math.floor((totalMinutes - days * 1440) / 60);
+  let minutes = Math.floor(totalMinutes - (days * 1440) - (hours * 60))
+  let formattedBingeTime = `${days} days, ${hours} hours, and ${minutes} minutes`
+
   return(
     <div>
       <h1>{props.singleShowData.original_name}</h1>
+      <p>{props.singleShowData.overview}</p>
+      <p>{props.singleShowData.original_name} has {props.singleShowData.number_of_seasons} seasons consisting of {props.singleShowData.number_of_episodes} episodes.</p>
+      <p>It will approximately take you {formattedBingeTime} to watch all of {props.singleShowData.original_name}</p>
     </div>
   )
 }
