086576b2daee8a9695de37c8b9cbd168d21da636
Philip Zak <zakphi@gmail.com>
2017-09-16 15:34:21 -0400

    restructure result component to display show poster

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import image from '../imgComingSoon.png'
 
 const Result = props => {
+  let base_url = props.configResults.secure_base_url
+  let image_size = 'w185'
+  let poster_path = props.result.poster_path
+  let poster_image = props.result.poster_path === null ? image : `${base_url}${image_size}${poster_path}`
+
   return(
     <div>
       <Link
         to={`/tv_shows/${props.result.id}`}
         onClick={ () => {props.handleSingleShowSearch(props.result.id)} }>
-          {props.result.original_name}
+          <img
+              src={poster_image}
+              alt={props.result.original_name}
+          />
         </Link>
     </div>
   )
