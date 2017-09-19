236fcf8ca24b6cfc2dfb3f9d5d14a8c3f70e7b76
Philip Zak <zakphi@gmail.com>
2017-09-19 18:48:15 -0400

    add logo to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from '../tmdb-logo.png'
 
 const Footer = () => {
   return(
     <footer>
+      <img src={logo} alt='tmdb logo' />
       <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
     </footer>
   )
