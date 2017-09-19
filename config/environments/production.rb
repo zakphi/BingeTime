c98b33705e7dece1ae92443f8211625d12a5ab3e
Philip Zak <zakphi@gmail.com>
2017-09-19 15:54:30 -0400

    possible fix for heroku issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # https://stackoverflow.com/a/26541742
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
