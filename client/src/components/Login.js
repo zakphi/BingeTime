b86698ffd14d19ea301cb0c6629e0eb4d30a6beb
Philip Zak <zakphi@gmail.com>
2017-10-06 14:19:54 -0400

    move login form into a section

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,23 +2,25 @@ import React from 'react'
 
 const Login = props => {
   return(
-    <form className='loginForm' onSubmit={props.handleLoginSubmit}>
-      <input
-        type='text'
-        name='loginUsername'
-        value={props.loginUsername}
-        placeholder='Username'
-        onChange={props.handleInputChange}
-      />
-      <input
-        type='password'
-        name='loginPassword'
-        value={props.loginPassword}
-        placeholder='Password'
-        onChange={props.handleInputChange}
-      />
-      <input type='submit' value='login' />
-    </form>
+    <section id='login'>
+      <form onSubmit={props.handleLoginSubmit}>
+        <input
+          type='text'
+          name='loginUsername'
+          value={props.loginUsername}
+          placeholder='Username'
+          onChange={props.handleInputChange}
+        />
+        <input
+          type='password'
+          name='loginPassword'
+          value={props.loginPassword}
+          placeholder='Password'
+          onChange={props.handleInputChange}
+        />
+        <input type='submit' value='login' />
+      </form>
+    </section>
   )
 }
 
