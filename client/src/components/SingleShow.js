20570effdce3c5a2f2ba876919d1a6e9451dac1c
Philip Zak <zakphi@gmail.com>
2017-10-06 15:09:26 -0400

    add missing period

--- client/src/components/SingleShow.js
+++ client/src/components/SingleShow.js
@@ -27,7 +27,7 @@ const SingleShow = props => {
         <h2>{props.singleShowData.original_name}</h2>
         <p>{props.singleShowData.overview}</p>
         <p>{props.singleShowData.original_name} has {props.singleShowData.number_of_seasons} seasons consisting of {props.singleShowData.number_of_episodes} episodes.</p>
-        <p>It will approximately take you {formattedBingeTime} to watch all of {props.singleShowData.original_name}</p>
+        <p>It will approximately take you {formattedBingeTime} to watch all of {props.singleShowData.original_name}.</p>
       </article>
       {props.auth
         ? <article id='showManageBtns'>
